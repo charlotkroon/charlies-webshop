d1d6e14b57f1e8ab99f3b279ed260203dd2105ad
charliegabriella <charlotroucha@protonmail.com>
2020-06-17 13:43:58 +0200

    Import react-router-dom and create exact paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import {Route} from "react-router-dom";
 import "./App.css";
 
 import Directory from "./components/directory/directory";
+import HomePage from "./pages/homepage/homepage";
+
+const HatsPage = () => (
+  <div>
+    <h1>hats page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className='App'>
+    <div>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
       <Directory />
     </div>
   );
