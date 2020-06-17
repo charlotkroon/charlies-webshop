6142f8d820a4e609325b3a94bd806e88bf1beb3c
charliegabriella <charlotroucha@protonmail.com>
2020-06-17 13:47:25 +0200

    Add Switch / Add Directory to HomePage component.

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,29 +1,10 @@
 import React from "react";
+import Directory from "../../components/directory/directory.js";
 import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className='homepage'>
-    <div className='directory-menu'>
-      <div className='content'>
-        <h1 className='title'>HATS</h1>
-        <span className='subtitle'>SHOP NOW</span>
-      </div>
-
-      <div className='content'>
-        <h1 className='title'>JACKETS</h1>
-        <span className='subtitle'>SHOP NOW</span>
-      </div>
-
-      <div className='content'>
-        <h1 className='title'>WOMENS</h1>
-        <span className='subtitle'>SHOP NOW</span>
-      </div>
-
-      <div className='content'>
-        <h1 className='title'>MENS</h1>
-        <span className='subtitle'>SHOP NOW</span>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
