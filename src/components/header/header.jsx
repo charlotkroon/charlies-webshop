b4c38d038a4ad77a30632f58294fc96ccd04f0bb
charliegabriella <charlotroucha@protonmail.com>
2020-06-25 11:04:45 +0200

    Create div. Export Header Component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "./header.styling.scss";
+
+const Header = () => (
+  <div className='header'>
+    <Link to='/'></Link>
+  </div>
+);
+
+export default Header;
