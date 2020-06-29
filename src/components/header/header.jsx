4f37ea9cd9fd77cf310deb48aca1f804347885aa
charliegabriella <charlotroucha@protonmail.com>
2020-06-29 11:16:03 +0200

    add logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import "./header.styling.scss";
+import {ReactComponent as Logo} from "../../assets/crown.svg";
 
 const Header = () => (
   <div className='header'>
