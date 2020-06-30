a2bba265010c662b62b4bc40dd39e83549266f14
charliegabriella <charlotroucha@protonmail.com>
2020-06-30 17:40:45 +0200

    Create functional component, add div and set up form

--- src/components/form-input/form-input.component.js
+++ src/components/form-input/form-input.component.js
@@ -1,2 +1,10 @@
 import React from "react";
 import "./form-input.styles.scss";
+
+const FormInput = ({handleChange, label, ...otherProps}) => (
+  <div className='group'>
+    <Input className='form-input' onChange={handleChange} />
+  </div>
+);
+
+export default FormInput;
