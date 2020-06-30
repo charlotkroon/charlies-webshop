29b7f859fe32d58bf8eb735e56baebd4c46d4dbe
charliegabriella <charlotroucha@protonmail.com>
2020-06-30 12:06:00 +0200

    Create sign in component

---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sign-in-sign-up.styles.scss";
+
+const SignInAndSignUpPage = () => (
+  <div className='sign-in-and-sign-up'>'SIGN IN SIGN UP'</div>
+);
+
+export default SignInAndSignUpPage;
