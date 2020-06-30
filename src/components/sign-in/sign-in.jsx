55e047402bf1ea82afcb185be38e7d320e0de17b
charliegabriella <charlotroucha@protonmail.com>
2020-06-30 12:10:13 +0200

    Create Sign In Form

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -17,8 +17,22 @@ class SignIn extends React.Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
-        <form></form>
+        <form>
+          <input name='email' type='email' value={this.state.email} required />
+          <label>Email</label>
+          <input
+            name='password'
+            type='password'
+            value={this.state.password}
+            required
+          />
+          <label>Password</label>
+
+          <input type='submit' value='Submit form' />
+        </form>
       </div>
     );
   }
 }
+
+export default SignIn;
