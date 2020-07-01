1d95fc51aa95b3824b87d20f515f2bf8857b7b83
charliegabriella <charlotroucha@protonmail.com>
2020-07-01 12:36:55 +0200

    Import Auth / Change Function to Class Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from "react";
-import {Switch, Route} from "react-router-dom";
-
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
-
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructur() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div >
+    );
+  }
+
 }
 
 export default App;
