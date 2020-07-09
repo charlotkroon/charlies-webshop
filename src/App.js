a35f46eb6e99d3ab54bf70e8b740a8b7714aa6b7
charliegabriella <charlotroucha@protonmail.com>
2020-07-09 15:26:37 +0200

    Add mapStateToProps and fix imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+//libraries and dependecies
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
 
+//actions
 import { setCurrentUser } from './redux/user/user.actions';
 
+//pages and components
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
@@ -16,11 +19,9 @@ class App extends React.Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
-
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
             id: snapShot.id,
@@ -50,8 +51,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser = user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
