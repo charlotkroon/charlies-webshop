0fc0caa0a0edebb555650868b4e0d3deeccdba1c
charliegabriella <charlotroucha@protonmail.com>
2020-07-09 15:12:34 +0200

    Write mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { connect } from 'react-redux';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
+
+import { setCurrentUser } from './redux/user/user.actions';
+
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
@@ -54,4 +50,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
