4967b2e0f05ed8900bfdc8a9d67de573817ae12e
charliegabriella <charlotroucha@protonmail.com>
2020-07-10 19:27:27 +0200

    Import CartIcon to Header Component // Fix formatting with formatter

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,36 +1,41 @@
-import React from "react";
-import { auth } from '../../firebase/firebase.utils.js'
-import { Link } from "react-router-dom";
+//libraries and dependecies
+import React from 'react';
+import { auth } from '../../firebase/firebase.utils.js';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
-import "./header.styling.scss";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
-
+//components
+import './header.styling.scss';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component.jsx';
 
 const Header = ({ currentUser }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo ' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
-      </Link>
-      <Link className='option' to='/contact'>
-        CONTACT
-      </Link>
-      {
-        currentUser ?
-          <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-          :
-          <Link className='option' to='/signin'>SIGN IN</Link>
-      }
-    </div>
-  </div>
+	<div className='header'>
+		<Link className='logo-container' to='/'>
+			<Logo className='logo ' />
+		</Link>
+		<div className='options'>
+			<Link className='option' to='/shop'>
+				SHOP
+			</Link>
+			<Link className='option' to='/contact'>
+				CONTACT
+			</Link>
+			{currentUser ? (
+				<div className='option' onClick={() => auth.signOut()}>
+					SIGN OUT
+				</div>
+			) : (
+				<Link className='option' to='/signin'>
+					SIGN IN
+				</Link>
+			)}
+			<CartIcon />
+		</div>
+	</div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = (state) => ({
+	currentUser: state.user.currentUser,
 });
 
 export default connect(mapStateToProps)(Header);
