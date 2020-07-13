010746195bd86070df04aa0961d563b5fa58f715
charliegabriella <charlotroucha@protonmail.com>
2020-07-13 14:22:26 +0200

    Add selectCartTotal selector, add redux to checkout component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -10,3 +10,10 @@ export const selectCartHidden = createSelector([selectCart], (cart) => cart.hidd
 export const selectedCartItemsCount = createSelector([selectCartItems], (cartItems) =>
 	cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0)
 );
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+	cartItems.reduce(
+		(accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity * cartItem.price,
+		0
+	)
+);
