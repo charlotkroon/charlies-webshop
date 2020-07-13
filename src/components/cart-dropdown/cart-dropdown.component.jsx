e191ef29c23817090f8ebead646d9f0ea0241995
charliegabriella <charlotroucha@protonmail.com>
2020-07-13 14:51:36 +0200

    Add checkout item component and styles. Update cart dropdown and checkoutpage.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import {selectCartItems} from '../../redux/cart/cart.selectors';
+import {toggleCartHidden} from '../../redux/cart/cart.actions';
 import {createStructuredSelector} from 'reselect';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
-const CartDropDown = ({cartItems, history}) => (
+const CartDropDown = ({cartItems, history, dispatch}) => (
 	<div className='cart-dropdown'>
 		<div className='cart-items'>
 			{cartItems.length ? (
@@ -16,7 +17,13 @@ const CartDropDown = ({cartItems, history}) => (
 				<span className='empty-message'>Your cart is empty</span>
 			)}
 		</div>
-		<CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+		<CustomButton
+			onClick={() => {
+				history.push('/checkout');
+				dispatch(toggleCartHidden());
+			}}>
+			GO TO CHECKOUT
+		</CustomButton>
 	</div>
 );
 
--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
 import './checkout.styles.scss';
+import CheckOutItem from '../../components/checkout-item/checkout-item.component';
 
 const CheckoutPage = ({cartItems, total}) => (
 	<div className='checkout-page'>
@@ -23,10 +24,12 @@ const CheckoutPage = ({cartItems, total}) => (
 				<span>Remove</span>
 			</div>
 		</div>
-		{cartItems.map((cartItem) => cartItem.name)}
+		{cartItems.map((cartItem) => (
+			<CheckOutItem key={cartItem.id} cartItem={cartItem} />
+		))}
 
 		<div className='total'>
-			<span>{total}</span>
+			<span>TOTAL: €{total}</span>
 		</div>
 	</div>
 );
