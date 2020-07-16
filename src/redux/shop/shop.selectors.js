f7c8cb4d127ee505538abfb51f2f3e51a45d8f19
charliegabriella <charlotroucha@protonmail.com>
2020-07-16 17:43:14 +0200

    Collection Routing and Selector

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,13 @@
 import {createSelector} from 'reselect';
 
+const COLLECTION_ID_MAP = {
+	hats: 1,
+	sneakers: 2,
+	jackets: 3,
+	womens: 4,
+	mens: 5,
+};
+
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector([selectShop], (shop) => shop.collections);
@@ -9,4 +17,6 @@ export const selectCollectionsForPreview = createSelector([selectCollections], (
 );
 
 export const selectCollection = (collectionUrlParam) =>
-	createSelector([selectCollections], (collections) => collections[collectionUrlParam]);
+	createSelector([selectCollections], (collections) =>
+		collections.find((collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+	);
