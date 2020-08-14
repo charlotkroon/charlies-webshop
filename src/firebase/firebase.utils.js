c588aa0158080daac3cf38a51cb2ef74b491da3f
charliegabriella <charlotroucha@protonmail.com>
2020-08-14 14:17:19 +0200

    Edit firebase data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 const config = {
 	apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-	authDomain: 'crwn-db.firebaseapp.com',
-	databaseURL: 'https://crwn-db.firebaseio.com',
-	projectId: 'crwn-db',
-	storageBucket: 'crwn-db.appspot.com',
-	messagingSenderId: '850995411664',
-	appId: '1:850995411664:web:7ddc01d597846f65',
+	authDomain: 'charlies-clothing.firebaseapp.com',
+	databaseURL: 'https://charlies-clothing.firebaseio.com',
+	projectId: 'charlies-clothing',
+	storageBucket: 'charlies-clothing.appspot.com',
+	messagingSenderId: '99519636328',
+	appId: '1:99519636328:web:1179b90e0e3a1c8c16ecdd',
 };
 firebase.initializeApp(config);
 export const createUserProfileDocument = async (userAuth, additionalData) => {
